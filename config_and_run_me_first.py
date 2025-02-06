dd3a9d5f5d76715eafc5896023b86fe021059bef
Kip <jeretech.jeremy@gmail.com>
2025-02-06 21:01:57 +0800

    Pycharm goofed up and didn't commit these changes :)

--- config_and_run_me_first.py
+++ config_and_run_me_first.py
@@ -1,6 +1,8 @@
-#===CHANGE=ME===#
-import sys
+# === READ ME ===
+# FIRST, Modify THIS file and run this to get the UUID!
+# THEN, modify and run config_and_run_me_second.py!
 
+#===CHANGE=ME===#
 # Relative directory to API key file. You can get this file from
 # Access > Users > [a user with enought permission] > API Keys
 api_file = "api_key.txt"
@@ -9,9 +11,8 @@ api_file = "api_key.txt"
 opnsense_host = "192.168.42.1"
 
 #==CODE==#
-import sys
 import requests
-from os import path, listdir
+import sys
 
 # Read api key
 with open(api_file) as f:
@@ -28,6 +29,18 @@ print(f"Going to {opnsense_api}")
 session = requests.Session()
 session.auth = (api_key, api_secret)
 
-# Interactive locator
-response = session.get(f"{opnsense_api}/wireguard/client/list_servers", verify=False)
-print(response.json())
+# locator
+try:
+    response = session.get(f"{opnsense_api}/wireguard/client/list_servers", verify=False)
+    response_json = response.json()
+    # print(response_json)
+    if response_json["status"] != "ok":
+        raise Exception(f"JSON response is not OK! ({response_json})")
+except Exception as e:
+    print(f"Something went wrong during API call. Make sure the API key is correct and it can access wireguard. ({e})")
+    sys.exit(-1)
+
+print("Name\tUUID")
+print("__________")
+for row in response_json["rows"]:
+    print(row["name"] + "\t" + row["uuid"])
